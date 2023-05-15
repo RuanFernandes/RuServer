65e4432429f4fad1ba5b1565f3611f6d8d987d7b
Ruan <ruan_guimaraes2004@hotmail.com>
2023-05-15 00:01:25 -0300

    Exemplo com Parametros/Query/Body

--- examples/HelloWorld.ts
+++ examples/HelloWorld.ts
@@ -1,9 +1,32 @@
-import { Controller, Get } from '..';
+import { Controller, Get, Post, RequestData } from '..';
 
 @Controller()
 export class HelloWorld {
+    // Can get request data too
+    // @Method()
+    // func(reqData: RequestData)
+
     @Get('hello', 'Hello World')
-    getTest() {
+    getTest(reqData: RequestData) {
+        if (reqData.query.name) {
+            return `Hello ${reqData.query.name}!`;
+        }
+
         return 'Hello!';
+    }
+
+    @Get('hello/:name', 'Hello World')
+    getTestParams(reqData: RequestData) {
+        return `Hello ${reqData.params.name}!`;
+    }
+
+    @Post('hello', 'Hello World')
+    postTest() {
+        return 'Hello Post!';
+    }
+
+    @Post('hello/data', 'Hello World')
+    postTestData(reqData: RequestData) {
+        return `Hello ${reqData.body.name}!`;
     }
 }
